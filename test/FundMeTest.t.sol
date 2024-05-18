dfccbd9aa3528a26a560f42b16832d476e933e59
AndreaDeBlasio55 <39781342+AndreaDeBlasio95@users.noreply.github.com>
2024-05-18 14:38:27 +0200

    new tests

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -4,15 +4,21 @@ pragma solidity ^0.8.24;
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
 import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
 
     function setUp() external {
         //fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         // In this way we only need to edit out DeployFundMe.s.sol script to update our test class in the exact same way we would update our FundMe contract.
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+
+        vm.deal(USER, STARTING_BALANCE);
     }
 
     function testMinimumDollarIsFive() public view {
@@ -24,14 +30,84 @@ contract FundMeTest is Test {
     // Before the DeployFundMe refactoring: us -> DeployFundMe (FundMe) -> FundMeTest
     // this is why now using msg.sender in the assertEq
     function testOwnerIsMsgSender() public view {
-        console.log("Owner: ", fundMe.i_owner());
-        console.log("Msg Sender: ", msg.sender);
-        assertEq(fundMe.i_owner(), msg.sender);
+        console.log("Owner: ", fundMe.getOwner());
+        assertEq(fundMe.getOwner(), msg.sender);
     }
 
     function testPriceFeedVersionIsAccurate() public view {
         // this will fail because foundry will run a local Anvil chain so that contract that we specify doesn't exist.
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
+    }
+
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert();
+        fundMe.fund();
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.startPrank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        vm.stopPrank();
+
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
+    }
+
+    function testAddsFunderToArrayOfFunders() public funded {
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, USER);
+    }
+
+    modifier funded() {
+        vm.startPrank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        vm.stopPrank();
+        _;
+    }
+
+    function testOnlyOwnerCanWithdraw() public funded {
+        // vm.expectRevert() will ignore the vm code after it, so the next line: fundMe.withdraw(), should revert
+        vm.expectRevert();
+        vm.startPrank(USER);
+        fundMe.withdraw();
+        vm.stopPrank();
+    }
+
+    function testWithdrawWithASingleFunder() public funded {
+        // arrange
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        // Act
+        vm.startPrank(fundMe.getOwner());
+        fundMe.withdraw();
+
+        // Assert
+        uint256 endingOwnerBalance = fundMe.getOwner().balance;
+        //uint256 endingFundMeBalance = address(fundMe).balance;
+        assertEq(startingFundMeBalance + startingOwnerBalance, endingOwnerBalance);
+    }
+
+    function testWithdrawFromMultupleFunders() public funded {
+        // uint160 have the same byte size as address, and solidity doesn't allow us to cast uint256 to address
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 1; // because 0 address(0) sometimes reverts
+        for (uint160 i = startingFunderIndex; i < numberOfFunders; i++) {
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        // Act
+        vm.startPrank(fundMe.getOwner());
+        fundMe.withdraw();
+        vm.stopPrank();
+
+        // Assert
+        assert(address(fundMe).balance == 0);
+        assert(startingFundMeBalance + startingOwnerBalance == fundMe.getOwner().balance);
     }
 }
